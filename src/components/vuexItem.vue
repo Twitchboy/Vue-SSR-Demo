fc60fa961a8e0c140f0f632a5db52ea4014cebb1
Junting <342766475@qq.com>
2018-06-27 12:34:54 +0800

    Store代码分割，动态注册模块

--- src/components/vuexItem.vue
+++ src/components/vuexItem.vue
@@ -5,21 +5,41 @@
     <p>
       item: {{ getItem().text }}
     </p>
+    <p>动态注册模块：{{fooCount}}</p>
   </div>
 </template>
 
 <script>
-import { mapState, mapGetters } from 'vuex'
+import { mapState, mapGetters, createNamespacedHelpers } from 'vuex'
+import fooStoreModule from '../store/modules/foo.js';
 
   export default {
     name: 'vuexItem',
     // 静态函数, 以便外部可以任意调用
     asyncData ({ store, route }) {
-      // 触发 action 后，会返回 Promise
-      return store.dispatch('fetchItem', route.params.id)
+      // 模块动态注册，需要才加载进来
+      store.registerModule('foo', fooStoreModule)
+
+      // 触发 action 后，会返回 Promise, 确保所有都执行完
+      return Promise.all([
+        store.dispatch('fetchItem', route.params.id),
+        store.dispatch('foo/inc')
+      ])
+    },
+    // 重要信息：当多次访问路由时，
+    // 避免在客户端重复注册模块。
+    destroyed () {
+      // 卸载模块
+      this.$store.unregisterModule('foo')
     },
     computed: {
-      ...mapState(['items'])
+      // fooCount () {
+      //   return this.$store.state.foo.count
+      // },
+      ...mapState({
+        items: "items",
+        fooCount: state => state.foo.count
+      }),
     },
     methods: {
       ...mapGetters({
